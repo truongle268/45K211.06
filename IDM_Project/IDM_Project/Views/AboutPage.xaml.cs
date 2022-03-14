e66f839d6bbc5bd972ff4bfd56e3620367704100
truongle268 <191121521151@due.udn.vn>
2022-03-14 23:57:41 +0700

    Create AboutPage.xaml.cs

---
+++ IDM_Project/IDM_Project/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IDM_Project.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
