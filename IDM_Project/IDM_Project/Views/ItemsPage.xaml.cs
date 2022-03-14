f47126837e7236439aca7dd3b129c3c1af0f2c8a
truongle268 <191121521151@due.udn.vn>
2022-03-14 23:57:54 +0700

    Create ItemsPage.xaml.cs

---
+++ IDM_Project/IDM_Project/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using IDM_Project.Models;
+using IDM_Project.ViewModels;
+using IDM_Project.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IDM_Project.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
