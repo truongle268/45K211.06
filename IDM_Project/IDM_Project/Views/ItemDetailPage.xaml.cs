3a1ab4088f3bccb3fa452d31fa836b07699c5a0e
truongle268 <191121521151@due.udn.vn>
2022-03-14 23:57:47 +0700

    Create ItemDetailPage.xaml.cs

---
+++ IDM_Project/IDM_Project/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using IDM_Project.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace IDM_Project.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
