66b3d3464e8550fc4631bc101753dba758d9cd84
truongle268 <191121521151@due.udn.vn>
2022-03-14 23:55:19 +0700

    Create App.xaml.cs

---
+++ IDM_Project/IDM_Project/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using IDM_Project.Services;
+using IDM_Project.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IDM_Project
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
