477e11efeb09aef71466e8b343e17c65a148a50b
HoangQuynh1410 <htq14102001@gmail.com>
2022-03-22 16:49:09 +0700

    UPdate source code

---
+++ IMD/IMD/IMD/IMD/LoginUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IMD
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtUsername.Text=="admin" && txtPassword.Text=="123456")
+            {
+                Navigation.PushAsync(new Home());
+            }
+            else
+            {
+                DisplayAlert("Alert", "Tên đăng nhập hoặc mật khẩu không đúng!", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Signin());
+        }
+    }
+}
---
+++ IMD/IMD/IMD/IMD/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMD.Models
+{
+    public class User
+    {
+        public string Bsx { get; set; }
+        public string Hovaten { get; set; }
+        public int Msv { get; set; }
+        public string Dongxe { get; set; }
+        public string Mauxe { get; set; }
+        public string Password { get; set; }
+
+    }
+}
